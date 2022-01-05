d4ce74dabda7625fede6853a3a5aa00f54151cef
Irina Kirillova <keira.kirillova@gmail.com>
2022-01-05 23:33:35 +0200

    add AppHeader elements

--- src/App/AppHeader.js
+++ src/App/AppHeader.js
@@ -1,19 +1,39 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Header = styled.header`
     display: flex;
+    align-items: center;
 `;
 
 const Logo = styled.header`
     margin-right: auto;
+    font-size: 32px;
+    font-weight: 600;
 `;
+
+const NavButtonElem = styled.div`
+    margin-left: 30px;
+    font-size: 16px;
+    line-height: 1.5;
+    cursor: pointer;
+    ${props => props.active && css`
+        color: #AB54DB;
+        cursor: default;
+    `}
+`;
+
+function NavButton({ name, active }) {
+    return (
+        <NavButtonElem active={active}>{name}</NavButtonElem>
+    )
+}
 
 export function AppHeader() {
     return (
         <Header>
             <Logo>Crypto</Logo>
-            <div>Dashboard</div>
-            <div>Settings</div>
+            <NavButton name="Dashboard" active />
+            <NavButton name="Settings" />
         </Header>
     )
 }
