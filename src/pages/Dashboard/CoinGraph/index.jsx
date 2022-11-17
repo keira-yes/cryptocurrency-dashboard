0c59548f1bfe25cdaedc173b69f70fff81597f39
Irina Kirillova <keira.kirillova@gmail.com>
2022-11-17 18:31:56 +0100

    add charts to coin info

--- src/pages/Dashboard/CoinGraph/index.jsx
+++ src/pages/Dashboard/CoinGraph/index.jsx
@@ -1,5 +1,22 @@
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+const options = {
+    title: {
+        text: 'My chart'
+    },
+    series: [{
+        data: [1, 2, 3]
+    }]
+}
+
 export const CoinGraph = () => {
     return (
-        <div>Graph</div>
+        <div>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={options}
+            />
+        </div>
     )
 }
